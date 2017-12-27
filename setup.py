db15c57f59f2c812074d76b64dcb43affde76187
sven <sven.fillinger@mailbox.org>
2017-12-27 10:49:01 +0100

    Change version to development status

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mtbparser',
-    version='0.1',
+    version='0.1dev',
     description='A module that parses different diagnostic variant data.',
     author='Sven Fillinger',
     author_email='sven.fillinger@qbic.uni-tuebingen.de',
