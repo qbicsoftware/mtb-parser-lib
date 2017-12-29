bd23d22462ddc8629b893a63375ac270a181b75e
Sven F <sven.fillinger@qbic.uni-tuebingen.de>
2017-12-29 10:20:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,21 @@ from setuptools import setup
 
 setup(
     name='mtbparser',
-    version='0.1dev',
+    version='0.1',
     license='MIT',
+    url='https://github.com/qbicsoftware/qbic.mtbparser'
     description='A module that parses different diagnostic variant data.',
     long_description=open('README.md').read(),
     author='Sven Fillinger',
     author_email='sven.fillinger@qbic.uni-tuebingen.de',
-    packages=['mtbparser']
+    packages=['mtbparser'],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Intended Audience :: Developers in tumor diagnostics",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "License :: OSI Approved :: MIT License"
+    ]
 )
