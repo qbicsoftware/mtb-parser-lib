a2fcb3883e13eb3574b446a26e42cacbe3a9703a
sven <sven.fillinger@mailbox.org>
2017-12-29 11:20:54 +0100

    Update description

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     license='MIT',
     url='https://github.com/qbicsoftware/qbic.mtbparser',
     description='A module that parses different diagnostic variant data.',
-    long_description=open('README.md').read(),
+    long_description=open('DESCRIPTION.rst').read(),
     author='Sven Fillinger',
     author_email='sven.fillinger@qbic.uni-tuebingen.de',
     packages=['mtbparser'],
